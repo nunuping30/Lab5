39a9ff714b4ab1eb01871e6fe21bf56545604ea8
nunuping30 <nooping2002@gmail.com>
2023-05-10 16:39:12 +0700

    full com but no

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,11 +51,11 @@ uint8_t TxBuffer[40];
 
 uint8_t FirstMenu[700] =
 			"===================================================== \r\n"
-			"                       Hello                          \r\n"
+			"                       Hello !!!                      \r\n"
 			"                Welcome to First Menu                 \r\n"
-			" Please selected only one choice which the menu have  \r\n"
+			"          Selected one choice in the menu have        \r\n"
 			"              Press the number of choice              \r\n"
-			"===================================================== \r\n"
+			"													   \r\n"
 			"                  0 : LED Control                     \r\n"
 			"                  1 : Button Status                   \r\n"
 			"===================================================== \r\n";
@@ -63,9 +63,9 @@ uint8_t FirstMenu[700] =
 uint8_t SecMenu[700] =
 			"===================================================== \r\n"
 			"               Welcome to Led Control                 \r\n"
-			" Please selected only one choice which the menu have  \r\n"
+			"          Selected one choice in the menu have        \r\n"
 			"             Press the Character of choice            \r\n"
-			"===================================================== \r\n"
+			"													   \r\n"
 			"                a : Speed Up   +1 Hz                  \r\n"
 			"                s : Speed Down -1 Hz                  \r\n"
 			"                d : On / Off LED                      \r\n"
@@ -76,7 +76,7 @@ uint8_t ThirdMenu[300] =
 			"===================================================== \r\n"
 			"              Welcome to Button Status                \r\n"
 			"       This Menu will show button status B1           \r\n"
-			"         Press 'x' to get to previous menu            \r\n"
+			"             Press 'x' to go to back menu             \r\n"
 			"===================================================== \r\n";
 
 //set
@@ -369,7 +369,7 @@ static void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 57600;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
